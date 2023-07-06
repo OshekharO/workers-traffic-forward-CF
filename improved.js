7103613623fd262c5f3406e3c2ce7f25d2a98478
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-06 22:41:15 +0530

    Update improved.js
    Signing off --- Saksham Shekher

--- improved.js
+++ improved.js
@@ -2,13 +2,13 @@ addEventListener('fetch', async (event) => {
   try {
     const url = new URL(event.request.url);
     url.protocol = 'http'; // It is recommended to use 'http' protocol instead of 'https' to avoid weird problems
-    url.hostname = 'connectvip.ml'; // In domain name, don't add http or https prefix, just the direct domain name
-    url.port = '10010'; // Port with http service
+    url.hostname = 'uncoder.eu.org'; // In domain name, don't add http or https prefix, just the direct domain name
+    url.port = '80'; // Port with http service
     url.pathname = '/test'; // Optional, turn the specific directory into the root directory, you don't need to delete or change it directly
     const request = new Request(url, event.request);
     const response = await fetch(request);
     event.respondWith(response);
   } catch (error) {
-    event.respondWith(new Response('An error occurred.', { status: 500 }));
+    event.respondWith(new Response(`An error occurred: ${error.message}`, { status: 500 }));
   }
 });
